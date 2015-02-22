e2ede5f2119166f35807de7b2fa58378252abccb
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-02-21 18:18:54 -0800

    Add form to contact page

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -16,6 +16,27 @@ define([
             return this;
         },
 
+        submit: function (event) {
+            event.preventDefault();
+
+            var input = this.$('input[name=email]')[0].value;
+            console.log(input);
+
+
+            var fields = this.$('form [name]');
+            var userInput = _.object(_.map(fields, function (field) {
+                return [field.name, field.value];
+            }));
+
+            console.log(userInput);
+            // TODO add url to enable saving
+            // this.model.set(userInput).save();
+        },
+
+        events: {
+            'submit form': 'submit',
+        },
+
     });
 
     return ContactView;
