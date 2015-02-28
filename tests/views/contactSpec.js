9121452dbe066b0ccbaf77df3396f9266181d640
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-02-27 16:15:48 -0800

    Add contact view tests
    contact view when constructing should have a model
    contact view when rendered should have content in el

--- tests/views/contactSpec.js
+++ tests/views/contactSpec.js
@@ -15,8 +15,22 @@ define([
                 expect(view).toBeDefined();
             });
 
+            it('should have a model', function() {
+                expect(view.model).toBeDefined();
+            });
+
             it('should have a template', function() {
                 expect(view.template).toBeDefined();
+            });
+        });
+
+        describe('when rendered', function () {
+            beforeEach(function () {
+                view.render();
+            });
+
+            it('should have content in el', function () {
+                expect(view.el.innerHTML).not.toEqual('');
             });
         });
     });
